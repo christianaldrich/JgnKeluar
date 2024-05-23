b7ba8345faa9ea6c4c0ee17ee6f9621afe8a2b9e
christianaldrich <pang.aldrich@gmail.com>
2024-05-23 23:59:55 +0700

    Initial Commit

---
+++ JgnKeluar Watch App/JgnKeluarApp.swift
@@ -0,0 +1,17 @@
+//
+//  JgnKeluarApp.swift
+//  JgnKeluar Watch App
+//
+//  Created by Christian Aldrich Darrien on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct JgnKeluar_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JgnKeluar/JgnKeluarApp.swift
@@ -0,0 +1,17 @@
+//
+//  JgnKeluarApp.swift
+//  JgnKeluar
+//
+//  Created by Christian Aldrich Darrien on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct JgnKeluarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
